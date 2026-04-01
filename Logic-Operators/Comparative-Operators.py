208432664930202e750c0bf2d186a636132bc876
Gerardo Avalos Sanchez <gerhard.avsn0901@gmail.com>
2026-03-31 21:08:55 -0600

    Add comparative operators examples in Python

---
+++ Logic-Operators/Comparative-Operators.py
@@ -0,0 +1,17 @@
+mi_bool = 5+5 == 18-8
+mi_bool2 = 'Blanco' == 'blanco'
+print(mi_bool)
+print(mi_bool2)
+
+mi_bool3 = '100' == 100
+print(mi_bool3)
+
+mi_bool4 = 100.0 == 100
+print(mi_bool4)
+
+mi_bool5 = 100 != 100
+print(mi_bool5)
+
+mi_bool6 = 100 > 99
+print(mi_bool6)
+
